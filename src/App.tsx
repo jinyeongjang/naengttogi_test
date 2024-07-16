f1f35c8e1ab44619e129c44d00a5f6c94b65e04e
jinyeongjang <wlsdud4222@gmail.com>
2024-07-16 11:22:42 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,26 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Login from "./pages/Login";
 import LoginToEmail from "./pages/LoginToEmail";
 
 function App() {
-  const handleLogin = (username: string, password: string) => {
-    console.log("Login attempt:", username, password);
-  };
-
-  const handleGoogleLogin = () => {
-    console.log("Google login attempt");
-  };
-
-  const handleEmailLogin = (email: string) => {
-    console.log("Email login attempt", email);
-  };
-
-  const handleForgotCredentials = () => {
-    console.log("Forgot credentials");
-  };
-
-  const handleSignUp = () => {
-    console.log("Sign up");
-  };
-
   return (
     <Router>
       <div className="App flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<Navigate to="/Login" replace />} />
             <Route
-              path="/Login"
+              path="/"
               element={
-                <Login
-                  onLogin={handleLogin}
-                  onGoogleLogin={handleGoogleLogin}
-                  onEmailLogin={() => handleEmailLogin("")}
-                  onForgotCredentials={handleForgotCredentials}
-                  onSignUp={handleSignUp}
-                />
+                <div>
+                  <h1>냉뚝이 테스트 페이지입니다.</h1>
+                </div>
               }
             />
-            <Route
-              path="/LoginToEmail"
-              element={
-                <LoginToEmail
-                  onLogin={handleEmailLogin}
-                  onGoogleLogin={handleGoogleLogin}
-                  onForgotCredentials={handleForgotCredentials}
-                  onSignUp={handleSignUp}
-                />
-              }
-            />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/LoginToEmail" element={<LoginToEmail />} />
           </Routes>
         </main>
         <NavBar />
